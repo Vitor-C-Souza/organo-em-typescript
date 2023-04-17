25a3d0bac8d14f66b4d1d84bf570be5f0f56d769
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-17 18:36:47 -0300

    migração do componente time para tsx

--- src/componentes/Time/index.tsx
+++ src/componentes/Time/index.tsx
@@ -1,24 +1,36 @@
-import Colaborador from '../Colaborador'
-import './Time.css'
+import { IColaborador } from "../../Compartilhado/Interface/IColaborador";
+import Colaborador from "../Colaborador";
+
+import "./Time.css";
 
 interface TimeProps {
-    corPrimaria: string
-    nome: string
-    colaboradores: 
+  corPrimaria: string;
+  corSecundaria: string;
+  nome: string;
+  colaboradores: IColaborador[];
 }
 
 const Time = (props: TimeProps) => {
-    const css = { backgroundColor: props.corSecundaria }
+  const css = { backgroundColor: props.corSecundaria };
 
-    return (
-        (props.colaboradores.length > 0) ? <section className='time' style={css}>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-            <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
-            </div>
-        </section> 
-        : ''
-    )
-}
+  return props.colaboradores.length > 0 ? (
+    <section className="time" style={css}>
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            corDeFundo={props.corPrimaria}
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+          />
+        ))}
+      </div>
+    </section>
+  ) : (
+    ""
+  );
+};
 
-export default Time
+export default Time;
